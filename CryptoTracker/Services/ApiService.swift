bf104679bae05d3448799d612599f4de1c974b98
Omar Elmanzalawy <omarmanz2002@gmail.com>
2025-06-17 21:52:43 +0300

    refactor code

--- CryptoTracker/Services/ApiService.swift
+++ CryptoTracker/Services/ApiService.swift
@@ -23,36 +23,14 @@ class ApiService{
     private func getCoins(){
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=250&sparkline=true&price_change_percentage=24h") else {return}
-        
-        //uses combine to fetch data from api and make it as a publisher
-        coinSubscription =  URLSession.shared.dataTaskPublisher(for: url)
-        //converts the data into json format and do task in background
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                print("data: \(output.data)")
-                guard let response = output.response
-                as? HTTPURLResponse,
-                response.statusCode >= 200 && response.statusCode < 300 else {
-                throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            //Move back to main thread to update UI
-            .receive(on: DispatchQueue.main)
+        coinSubscription =  NetworkingManager.downloadData(from: url)
             //decodes the json data into Coin model
             .decode(type: [Coin].self, decoder: JSONDecoder())
             //react to the results of decoding and updating the UI accordingly with completion handler
-            .sink {(completion) in
-                switch completion{
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(String(describing: error))
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 //cancel the subscription if we have received the coins as we don't need to keep listening for more updates.
                 self?.coinSubscription?.cancel()
-            }
+            })
     }
 }
---
+++ CryptoTracker/Utilities/NetworkingManager.swift
@@ -0,0 +1,61 @@
+//
+//  NetworkingManager.swift
+//  CryptoTracker
+//
+//  Created by MAC on 17/06/2025.
+//
+
+import Foundation
+import Combine
+
+class NetworkingManager{
+    
+    enum NetworkingError: LocalizedError{
+        case badURLResonse
+        case unkown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResonse:
+                return "Bad response from url"
+            case .unkown:
+                return "Unkown error occured"
+            }
+        }
+    }
+    
+    // uses Combine framework to handle network requests and responses and return the response as a publisher
+    static func downloadData(from url: URL) -> AnyPublisher<Data,Error> {
+        //uses combine to fetch data from api and make it as a publisher
+        return URLSession.shared.dataTaskPublisher(for: url)
+        //converts the data into json format and do task in background
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap ({ try handleUrlResponse(output: $0)})
+        
+            //Move back to main thread to update UI
+            .receive(on: DispatchQueue.main)
+            //return publisher of type Anypublisher so that specify a return type
+            .eraseToAnyPublisher()
+    }
+    
+    
+    static func handleUrlResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data{
+        print("data: \(output.data)")
+        guard let response = output.response
+        as? HTTPURLResponse,
+        response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResonse
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>?){
+        switch completion{
+        case .finished:
+            break
+        case .failure(let error):
+            print(String(describing: error))
+        default: break
+        }
+    }
+}
